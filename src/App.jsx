241dea0d65e2dc50687f7da3b061d48db511e835
Rjmhrzn-002 <rjmhrzn.143@gmail.com>
2024-02-09 23:00:34 +0545

    create:Modal and Login Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import Login from "./pages/auth/Login";
 import Button from "./components/button/Button";
-import Search from "./components/input/Search";
+import Modal from "./components/modal/Modal";
 
 function App() {
+  const [data, setData] = useState(999);
+  const [title, setTitle] = useState("Login");
+
+  const [isVisible, setVisible] = useState(false);
+
+  const handleClick = () => {
+    setData((d) => d + 1);
+    setTitle((d) => d + "e");
+  };
   return (
     <>
-      <div className=" w-md ">
-        <Button title="Submit" onClick={() => console.log("Clicked")} />
-        <Search />
+      <div className=" h-screen flex justify-center items-center">
+        My value is {data}.{/* <Login /> */}
+        <Button
+          title={title}
+          onClick={() => {
+            setVisible(true);
+          }}
+        />
+        <Modal isVisible={isVisible} onModalClick={() => setVisible(false)}>
+          <Login></Login>
+        </Modal>
       </div>
     </>
   );
---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,34 @@
+const Modal = ({ isVisible, onModalClick, children }) => {
+  if (!isVisible) {
+    return null;
+  }
+
+  const handleClose = (e) => {
+    if (e.target.id === "modalContainer") {
+      onModalClick();
+    }
+  };
+
+  return (
+    <>
+      <div
+        onClick={handleClose}
+        id="modalContainer"
+        className="absolute inset-0 bg-black bg-opacity-20 z-40 flex items-center justify-center"
+      >
+        <div className="flex justify-end">
+          {/* <button
+            onClick={() => onModalClick()}
+            className="bg-red-500 py-2 px-4 absolute right-0 top-0 "
+          >
+            X
+          </button> */}
+        </div>
+        <div className="w-xs">{children}</div>
+      </div>
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Modal;
