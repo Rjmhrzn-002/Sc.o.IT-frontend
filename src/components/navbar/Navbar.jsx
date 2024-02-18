2b989942c3fb7b3546690d4db0d4437ce13237e3
Rojen Maharjan <131804792+Rjmhrzn-002@users.noreply.github.com>
2024-02-18 17:27:46 +0545

    Feature/14 (#15)
    * create:navbar and upperNav
    
    * create:landing page and modification on few
    
    ---------
    
    Co-authored-by: Rjmhrzn-002 <rjmhrzn.143@gmail.com>

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,37 +1,44 @@
 import { CgMenuRightAlt } from "react-icons/cg";
-import { useNavigate } from "react-router";
-import navdata from "./navdata.js";
+import { NavLink } from "react-router-dom";
+import { navdata } from "./navdata.js";
 import UpperNav from "./UpperNav.jsx";
+import "./navbar.css";
 
 const Navbar = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
   return (
     <>
-      <div className="w-full  ">
-        <div>
-          <UpperNav></UpperNav>
-        </div>
-        <div className="max-w-[1280px] mx-4 md:mx-24 h-20 flex justify-between items-center text-white ">
-          <div className="flex items-center">
-            <div className="w-8 h-8 bg-primary rounded-full"></div>
-            <div className="uppercase font-bold text-2xl tex text-primary">
-              Logo
-            </div>
-          </div>
-
-          <div className="hidden md:flex items-center px-8 py-2 gap-8 font-semibold bg-primary rounded-md">
-            {navdata.map((nav) => (
-              <div
-                key={nav.id}
-                className=" px-4 py-2 cursor-pointer hover:bg-white hover:rounded-md hover:text-primary"
-                onClick={() => navigate(nav.path)}
-              >
-                {nav.title}
+      <div className="w-full shadow-md ">
+        <UpperNav></UpperNav>
+        <div className="max-w-[1280px] mx-auto ">
+          <div className="max-w-[1280px] mx-4 md:mx-24 h-20 flex justify-between items-center text-white ">
+            <div className="flex items-center gap-2">
+              <div className="w-10 h-10 bg-primary rounded-full"></div>
+              <div className="uppercase font-bold text-2xl tex text-primary">
+                Logo
               </div>
-            ))}
-          </div>
-          <div className="md:hidden">
-            <CgMenuRightAlt size={35} className="cursor-pointer" />
+            </div>
+
+            <div className="hidden md:flex items-center px-8 py-2 gap-8 font-semibold bg-primary rounded-md">
+              {navdata.map((nav) => (
+                <NavLink
+                  to={nav.path}
+                  key={nav.id}
+                  className="  px-4 py-2 cursor-pointer hover:bg-white hover:rounded-md hover:text-primary"
+                  // onClick={() => navigate(nav.path)}
+                >
+                  {nav.title}
+                </NavLink>
+              ))}
+            </div>
+            <div className="md:hidden">
+              <CgMenuRightAlt
+                size={35}
+                color="purple"
+                className="cursor-pointer"
+              />
+            </div>
           </div>
         </div>
       </div>
--- src/components/navbar/UpperNav.jsx
+++ src/components/navbar/UpperNav.jsx
@@ -1,11 +1,13 @@
-import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaInstagram, FaFacebook, FaTwitter, FaPhoneAlt } from "react-icons/fa";
 const UpperNav = () => {
   return (
     <>
-      <div className="w-full bg-zinc-200 ">
-        <div className="flex items-center justify-between max-w-[1280px] md:mx-24">
-          <div>📞 Call us: +977 9876543210</div>
+      <div className="w-full bg-zinc-200 border border-b-primary opacity-80 ">
+        <div className="flex items-center justify-between max-w-[1280px] mx-auto px-5">
+          <div className="flex items-center gap-2">
+            <FaPhoneAlt></FaPhoneAlt>
+            <p>+977 9876543210</p>
+          </div>
           <div className="flex items-center gap-4">
             <FaInstagram></FaInstagram>
             <FaFacebook></FaFacebook>
