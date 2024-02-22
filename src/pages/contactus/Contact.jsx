9527954c70861cfa30dfd569c4bcae0f732d3972
Rojen Maharjan <131804792+Rjmhrzn-002@users.noreply.github.com>
2024-02-22 12:29:18 +0545

    create:contact us page (#25)
    Co-authored-by: Rjmhrzn-002 <rjmhrzn.143@gmail.com>

--- src/pages/contactus/Contact.jsx
+++ src/pages/contactus/Contact.jsx
@@ -1,33 +1,63 @@
+import { FaUser, FaEnvelope, FaPhone } from "react-icons/fa";
+import Button from "../../components/button/Button";
 const Contact = () => {
   return (
     <div className="w-full bg-white">
-      <div className="max-w-[1080px] md:mx-auto mx-2  ">
-        <div className="flex justify-center items-center py-24">
-          <h2 className=" w-2/6 text-center uppercase font-extrabold text-4xl text-primary shadow-2xl">
-            Tell us want you want to shop...
+      <div className="max-w-[1280px] mx-2 md:mx-auto flex justify-center items-center py-24">
+        <h2 className=" w-2/6 text-center uppercase font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-tr from-primary to-secondary">
+          Tell us want you want to shop...
+        </h2>
+      </div>
+      <div className=" w-full bg-zinc-100 flex flex-col items-center">
+        <div className=" max-w-[1280px] mx-2 md:mx-auto">
+          <h2 className="w-full uppercase font-semibold text-xl text-zinc-700 ">
+            Get in Touch
           </h2>
-        </div>
-        <div className="bg-black flex flex-col items-center">
-          <h2 className="text-white text-xl w-max">Get in Touch</h2>
-          <form className="grid grid-cols-3 gap-6 w-full bg-red-800 py-8">
-            <input
-              type="text"
-              name="fullName"
-              id="fullName"
-              placeholder="Your name"
-            />
-            <input
-              type="text"
-              name="fullName"
-              id="fullName"
-              placeholder="Your name"
-            />
-            <input
-              type="text"
-              name="fullName"
-              id="fullName"
-              placeholder="Your name"
-            />
+          <form className="grid grid-cols-3 gap-4 w-full py-8">
+            <div className="flex items-center relative">
+              <FaUser color="gray" className="absolute left-3"></FaUser>
+              <input
+                type="text"
+                name="fullName"
+                id="fullName"
+                placeholder="Your name"
+                className="py-2 px-10 rounded-md w-full border border-gray-500 focus:outline-2 focus:border-none"
+                required
+              />
+            </div>
+            <div className="flex items-center relative">
+              <FaEnvelope color="gray" className="absolute left-3"></FaEnvelope>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Your email"
+                className="py-2 px-10 rounded-md w-full border border-gray-500 focus:outline-2 focus:border-none"
+                required
+              />
+            </div>
+            <div className="flex items-center relative ">
+              <FaPhone
+                color="gray"
+                className="absolute left-3 rotate-90"
+              ></FaPhone>
+              <input
+                type="text"
+                name="fullName"
+                id="mobile"
+                placeholder="Your mobile"
+                className="py-2 px-10 rounded-md w-full border border-gray-500 focus:outline-2 focus:border-none"
+                required
+              />
+            </div>
+            <div className="col-span-2">
+              <textarea
+                name="message"
+                placeholder="Your Message"
+                className="w-full p-4 rounded-md border border-gray-500 focus:outline-2 focus:border-none"
+              ></textarea>
+            </div>
+            <Button title="Submit"></Button>
           </form>
         </div>
       </div>
